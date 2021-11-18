2c03969e66d9ec5db73bb9e01546e59f73aba96d
LucasManuelRodriguez <rodriguez.m.lucas@gmail.com>
2021-11-18 08:56:10 -0300

    modified flie citaList

--- src/citaList.php
+++ src/citaList.php
@@ -1,10 +1,5 @@
 <?php
 require_once __DIR__ . '/../src/Cita.php';
 
-    $cita1 = new Cita('Facu','It Crowd','Servidor','como se enrutan las vistas');
-    $cita2 = new Cita('Rita','It Crowd','LocalHost','no me sale el num 8000');
-    $cita3 = new Cita('Meli','It Crowd','Funciones','que parametros tomar');
-    
-    
-    $listaCitas = array($cita1,$cita2,$cita3);
+    $listaCitas = array();
 ?>
