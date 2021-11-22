36780ab9cada1731bf074125cc1d9da52b513850
Carruor <fotoes10@gmail.com>
2021-11-22 12:24:10 +0100

    still working on routes

--- src/Controllers/CitaController.php
+++ src/Controllers/CitaController.php
@@ -2,4 +2,18 @@
 namespace App\Controllers;
 use App\Models\Cita;
 
+class CitaController {
+    
+    public function index() {
+        //$citas = (new Cita())->all();
+        require_once __DIR__ . '/../views/pages/citaList.php';
+        
+    }
+    public function create() {
+        require_once __DIR__ . '/../views/pages/create.php';
+    }
+
+
+}
+
 
--- src/routes.php
+++ src/routes.php
@@ -1,15 +1,21 @@
 <?php
 namespace App;
 use App\controllers\CitaController;
+use App\Models\Cita;
+
+require_once __DIR__ . '/Controllers/CitaController.php';
 
 $uri = $_SERVER["REQUEST_URI"];
 
 if ($uri == '/list'){
      require_once __DIR__ . '/../views/pages/citaList.php';
+     $controller = new CitaController();
+     $controller->index();
  }
 
 if ($uri == '/list/create'){
     require_once __DIR__ . '/../views/pages/create.php';
+    $controller->create();
 }
 
  if ($uri == 'list/modify'){
