5836991e9787c59b8b37cbb4d39f4395e77105b5
MeliMano <melimanoni@gmail.com>
2021-11-24 10:02:35 +0100

    config php pass

--- src/Core/SQLConexion.php
+++ src/Core/SQLConexion.php
@@ -9,10 +9,11 @@ class SQLConexion {
 
     public function __construct()
     {
-        $host='localhost';
-        $user='root';
-        $pass='';
-        $database='lista_citas';
+        $config = new Config();
+        $host = $config->host;
+        $user = $config->user;
+        $pass = $config->pass;
+        $database = $config->database;
         $charset='utf-8';
         $options = [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC];
         $this->mysql = new PDO ("mysql:host={$host}; dbname={$database};charset{$charset}",$user,$pass,$options); 
